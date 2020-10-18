aeaffbfb305e9cc814ce7a7b9415b36055c88738
Abhishek AN <darkabhi1520@gmail.com>
2020-10-19 04:14:31 +0530

    Convert to KT
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/iku/PasswordInputActivity.kt
+++ app/src/main/java/com/iku/PasswordInputActivity.kt
@@ -1,6 +1,5 @@
 package com.iku
 
-import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -8,159 +7,137 @@ import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import com.google.android.gms.tasks.Task
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.iid.InstanceIdResult
-import com.iku.PasswordInputActivity
+import com.google.firebase.ktx.Firebase
 import com.iku.databinding.ActivityPasswordInputBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class PasswordInputActivity : AppCompatActivity() {
-    private var binding: ActivityPasswordInputBinding? = null
-    private var firebaseAuth: FirebaseAuth? = null
-    private var user: FirebaseUser? = null
-    private var mFirebaseAnalytics: FirebaseAnalytics? = null
-    private var db: FirebaseFirestore? = null
-    private var mProgress: ProgressDialog? = null
+    private lateinit var binding: ActivityPasswordInputBinding
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mFirebaseAnalytics: FirebaseAnalytics
+    private lateinit var db: FirebaseFirestore
+    private lateinit var user: FirebaseUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPasswordInputBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
-        val extras = intent.extras
-        val enteredEmail = extras!!.getString("email")
-        firebaseAuth = FirebaseAuth.getInstance()
+        setContentView(binding.root)
+        mAuth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
+        mFirebaseAnalytics = Firebase.analytics
+        val email = intent.getStringExtra("email")
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
-        binding!!.enterPassword.requestFocus()
-        initProgressDialog()
-        binding!!.backButton.setOnClickListener { view: View? -> onBackPressed() }
-        binding!!.signinButton.setOnClickListener { view: View? ->
-            binding!!.signinButton.isEnabled = false
-            if (binding!!.enterPassword.text.toString().isEmpty()) {
-                binding!!.signinButton.isEnabled = true
-                Toast.makeText(this@PasswordInputActivity, "Please enter your password", Toast.LENGTH_SHORT).show()
+        binding.enterPassword.doOnTextChanged { _, _, _, _ -> binding.enterPasswordLayout.isErrorEnabled = false }
+        binding.enterPassword.requestFocus()
+        binding.backButton.setOnClickListener { onBackPressed() }
+        binding.signinButton.setOnClickListener {
+            progressBarView(true)
+            binding.signinButton.isEnabled = false
+            if (binding.enterPassword.text.toString().isEmpty()) {
+                progressBarView(false)
+                binding.signinButton.isEnabled = true
+                Toast.makeText(this, "Please enter your password.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             } else {
-                mProgress!!.show()
-                firebaseAuth!!.signInWithEmailAndPassword(enteredEmail!!, binding!!.enterPassword.text.toString())
-                        .addOnCompleteListener(this@PasswordInputActivity) { tasks: Task<AuthResult?> ->
-                            FirebaseInstanceId.getInstance().instanceId
-                                    .addOnCompleteListener { task: Task<InstanceIdResult> ->
-                                        if (!task.isSuccessful) {
-                                            return@addOnCompleteListener
-                                        }
-
-                                        // Get new Instance ID token
-                                        val token = task.result.token
-                                        if (tasks.isSuccessful) {
-                                            user = firebaseAuth!!.currentUser
-                                            val rootRef = FirebaseFirestore.getInstance()
-                                            val docIdRef = rootRef.collection("users").document(user!!.uid)
-                                            docIdRef.get().addOnCompleteListener { task1: Task<DocumentSnapshot> ->
-                                                if (task1.isSuccessful) {
-                                                    val document = task1.result
-                                                    if (document.exists()) {
-                                                        sendRegistrationToken(token, user!!.uid)
-                                                        Toast.makeText(this@PasswordInputActivity, "Login successful", Toast.LENGTH_LONG).show()
-                                                        //sending to Home Activity
-                                                        mProgress!!.dismiss()
-                                                        val goToHomeActivity = Intent(this@PasswordInputActivity, HomeActivity::class.java)
-                                                        goToHomeActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-                                                        startActivity(goToHomeActivity)
-
-                                                        //log event
-                                                        val password_bundle = Bundle()
-                                                        password_bundle.putString(FirebaseAnalytics.Param.METHOD, "Email")
-                                                        mFirebaseAnalytics!!.logEvent(FirebaseAnalytics.Event.LOGIN, password_bundle)
-                                                    } else {
-                                                        Toast.makeText(this@PasswordInputActivity, "User profile not filled!", Toast.LENGTH_LONG).show()
-                                                        //sending to NameInput Activity
-                                                        mProgress!!.dismiss()
-                                                        val goToNameInputActivity = Intent(this@PasswordInputActivity, NameInputActivity::class.java)
-                                                        goToNameInputActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-                                                        startActivity(goToNameInputActivity)
-                                                    }
-                                                } else {
-                                                    binding!!.signinButton.isEnabled = true
-                                                }
+                mAuth.signInWithEmailAndPassword(email.toString(), binding.enterPassword.text.toString()).addOnCompleteListener { tasks: Task<AuthResult?> ->
+                    if (tasks.isSuccessful) {
+                        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                // Token
+                                val token = it.result.token
+                                user = mAuth.currentUser!!
+                                db.collection("users").document(user.uid).get().addOnCompleteListener { task: Task<DocumentSnapshot> ->
+                                    if (task.isSuccessful) {
+                                        if (task.result.exists()) {
+                                            sendRegistrationToken(token, user.uid)
+                                            mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN) {
+                                                param(FirebaseAnalytics.Param.METHOD, "Email")
+                                                param("status", "login_successful")
                                             }
+                                            startActivity(Intent(this, HomeActivity::class.java).apply { addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK) })
                                         } else {
-                                            mProgress!!.dismiss()
-                                            binding!!.signinButton.isEnabled = true
-
-                                            //log event
-                                            val fail_bundle = Bundle()
-                                            fail_bundle.putString(FirebaseAnalytics.Param.METHOD, "Email")
-                                            fail_bundle.putString("failure_reason", "incorrect password")
-                                            mFirebaseAnalytics!!.logEvent(FirebaseAnalytics.Event.LOGIN, fail_bundle)
-                                            Toast.makeText(this@PasswordInputActivity, "Incorrect password", Toast.LENGTH_LONG).show()
+                                            mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN) {
+                                                param(FirebaseAnalytics.Param.METHOD, "Email")
+                                                param("user_returned", "completing_name_input")
+                                            }
+                                            startActivity(Intent(this, NameInputActivity::class.java).apply { addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK) })
                                         }
+                                    } else {
+                                        binding.signinButton.isEnabled = true
                                     }
+                                }
+                            }
                         }
+                    } else {
+                        progressBarView(false)
+                        binding.signinButton.isEnabled = true
+                        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN) {
+                            param(FirebaseAnalytics.Param.METHOD, "Email")
+                            param("failure_reason", "incorrect_password")
+                        }
+                        binding.enterPasswordLayout.error = "Incorrect Password!"
+                    }
+                }
             }
         }
-        binding!!.forgotPasswordTextView.setOnClickListener { view: View? ->
-            firebaseAuth!!.sendPasswordResetEmail(enteredEmail!!)
-                    .addOnSuccessListener { aVoid: Void? ->
-                        Toast.makeText(this@PasswordInputActivity, "Password reset instructions sent via email", Toast.LENGTH_LONG).show()
-                        //log event
-                        val passwordReset_bundle = Bundle()
-                        passwordReset_bundle.putString("state", "sent")
-                        mFirebaseAnalytics!!.logEvent("password_reset_email", passwordReset_bundle)
-                    }
-                    .addOnFailureListener { e: Exception ->
-                        Toast.makeText(this@PasswordInputActivity, "Email Not Sent" + e.message, Toast.LENGTH_LONG).show()
-                        //log event
-                        val failed_bundle = Bundle()
-                        failed_bundle.putString("state", "failed")
-                        mFirebaseAnalytics!!.logEvent("password_reset_email", failed_bundle)
-                    }
+        binding.forgotPasswordTextView.setOnClickListener {
+            mAuth.sendPasswordResetEmail(email.toString()).addOnSuccessListener {
+                Toast.makeText(this, "Password reset instructions sent via email", Toast.LENGTH_LONG).show()
+                mFirebaseAnalytics.logEvent("password_reset_email") {
+                    param("state", "password_reset_sent")
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, "Email not sent. Contact support.", Toast.LENGTH_LONG).show()
+                mFirebaseAnalytics.logEvent("password_reset_email") {
+                    param("state", "Failed_for " + user.uid)
+                }
+            }
             object : CountDownTimer(60000, 1000) {
                 override fun onTick(millisUntilFinished: Long) {
-                    binding!!.forgotPasswordTextView.isEnabled = false
-                    binding!!.forgotPasswordTextView.text = "Resend in " + SimpleDateFormat("ss").format(Date(millisUntilFinished)) + "s"
+                    binding.forgotPasswordTextView.isEnabled = false
+                    binding.forgotPasswordTextView.text = applicationContext.getString(R.string.resend_email_timer_text, SimpleDateFormat("ss", Locale.US).format(Date(millisUntilFinished)))
                 }
 
                 override fun onFinish() {
-                    binding!!.forgotPasswordTextView.text = "Forgot Password?"
-                    binding!!.forgotPasswordTextView.isEnabled = true
+                    binding.forgotPasswordTextView.isEnabled = true
+                    binding.forgotPasswordTextView.text = getString(R.string.forgot_password_question)
                 }
             }.start()
         }
     }
 
-    private fun initProgressDialog() {
-        mProgress = ProgressDialog(this)
-        mProgress!!.setTitle("Logging in..")
-        mProgress!!.setMessage("")
-        mProgress!!.setCancelable(false)
-        mProgress!!.isIndeterminate = true
+    override fun onResume() {
+        binding.signinButton.isEnabled = true
+        super.onResume()
     }
 
-    override fun onResume() {
-        super.onResume()
-        binding!!.signinButton.isEnabled = true
+    private fun progressBarView(value: Boolean) {
+        if (value) {
+            binding.progressBar.visibility = View.VISIBLE
+            window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        } else {
+            binding.progressBar.visibility = View.GONE
+            window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+        }
     }
 
     private fun sendRegistrationToken(token: String, uid: String) {
-        val userRegistrationTokenInfo: MutableMap<String, Any> = HashMap()
-        userRegistrationTokenInfo["registrationToken"] = token
-        userRegistrationTokenInfo["uid"] = uid
-        db!!.collection("registrationTokens").document(uid)
-                .set(userRegistrationTokenInfo)
-                .addOnSuccessListener { aVoid: Void? -> }
-                .addOnFailureListener { e: Exception? -> }
-    }
-
-    companion object {
-        private val TAG = PasswordInputActivity::class.java.simpleName
+        val data = mapOf(
+                "registrationToken" to token,
+                "uid" to uid
+        )
+        db.collection("registrationTokens").document(uid).set(data).addOnSuccessListener { }.addOnFailureListener { }
     }
 }
