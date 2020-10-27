bc6f5173fd4091dcacc9ed34cef4c7984c217f68
Abhishek AN <darkabhi1520@gmail.com>
2020-10-27 23:48:50 +0530

    HOF finished, if you come back to me with this its there for you
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- app/src/main/java/com/iku/ui/HallOfFameActivity.kt
+++ app/src/main/java/com/iku/ui/HallOfFameActivity.kt
@@ -1,17 +1,18 @@
 package com.iku.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.SnapHelper
+import com.iku.UserProfileActivity
 import com.iku.databinding.ActivityHallOfFameBinding
 import com.iku.viewmodel.HallOfFameViewModel
 import kotlinx.android.synthetic.main.activity_hall_of_fame.*
 
-
-class HallOfFameActivity : AppCompatActivity() {
+class HallOfFameActivity : AppCompatActivity(), HallOfFameAdapter.CellClickListener {
     private lateinit var adapter: HallOfFameAdapter
     private val viewModel by lazy { ViewModelProvider(this).get(HallOfFameViewModel::class.java) }
     private lateinit var binding: ActivityHallOfFameBinding
@@ -20,7 +21,7 @@ class HallOfFameActivity : AppCompatActivity() {
         binding = ActivityHallOfFameBinding.inflate(layoutInflater)
         setContentView(binding.root)
         back_button.setOnClickListener { onBackPressed() }
-        adapter = HallOfFameAdapter(this)
+        adapter = HallOfFameAdapter(this, this)
         val linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         linearLayoutManager.stackFromEnd = true
         hofRecyclerView.layoutManager = linearLayoutManager
@@ -29,10 +30,15 @@ class HallOfFameActivity : AppCompatActivity() {
         hofRecyclerView.adapter = adapter
         observeData()
     }
+
     private fun observeData() {
         viewModel.fetchWinnerData().observe(this, {
             adapter.setListData(it)
             adapter.notifyDataSetChanged()
         })
+    }
+
+    override fun onCellClickListener(name: String, uid: String) {
+        startActivity(Intent(this, UserProfileActivity::class.java).putExtra("EXTRA_PERSON_NAME", name).putExtra("EXTRA_PERSON_UID", uid))
     }
 }
--- app/src/main/java/com/iku/ui/HallOfFameAdapter.kt
+++ app/src/main/java/com/iku/ui/HallOfFameAdapter.kt
@@ -1,19 +1,21 @@
 package com.iku.ui
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.transform.CircleCropTransformation
+import com.amulyakhare.textdrawable.TextDrawable
 import com.iku.R
 import com.iku.models.WinnersModel
 import kotlinx.android.synthetic.main.hall_of_fame_data.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class HallOfFameAdapter(val context: Context) : RecyclerView.Adapter<HallOfFameAdapter.WinnersViewHolder>() {
+class HallOfFameAdapter(val context: Context, val cellClickListener: CellClickListener) : RecyclerView.Adapter<HallOfFameAdapter.WinnersViewHolder>() {
     private var dataList = mutableListOf<WinnersModel>()
     fun setListData(data: MutableList<WinnersModel>) {
         dataList = data
@@ -32,6 +34,10 @@ class HallOfFameAdapter(val context: Context) : RecyclerView.Adapter<HallOfFameA
         return dataList.size
     }
 
+    interface CellClickListener {
+        fun onCellClickListener(name: String, uid: String)
+    }
+
     inner class WinnersViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindView(winner: WinnersModel) {
             itemView.month.text = SimpleDateFormat("MMMM yyyy", Locale.US).format(winner.timestamp)
@@ -41,9 +47,48 @@ class HallOfFameAdapter(val context: Context) : RecyclerView.Adapter<HallOfFameA
             itemView.runnerUp1Hearts.text = winner.second["points"].toString()
             itemView.runnerUp2Name.text = winner.third["name"] as CharSequence
             itemView.runnerUp2Hearts.text = winner.third["points"].toString()
-            itemView.winnerImage.load(winner.first["imageUrl"].toString()) { transformations(CircleCropTransformation()) }
-            itemView.runnerUp1Image.load(winner.second["imageUrl"].toString()) { transformations(CircleCropTransformation()) }
-            itemView.runnerUp2Image.load(winner.third["imageUrl"].toString()) { transformations(CircleCropTransformation()) }
+            if (winner.first["imageUrl"]!=null)
+                itemView.winnerImage.load(winner.first["imageUrl"].toString()) { transformations(CircleCropTransformation()) }
+            else{
+                val firstLetter = winner.first["name"].toString()[0].toString()
+                val secondLetter = winner.first["name"].toString().substring(winner.first["name"].toString().indexOf(' ') + 1, winner.first["name"].toString().indexOf(' ') + 2).trim { it <= ' ' }
+                val drawable = TextDrawable.builder()
+                        .beginConfig()
+                        .width(200)
+                        .height(200)
+                        .endConfig()
+                        .buildRect(firstLetter + secondLetter, Color.DKGRAY)
+                itemView.winnerImage.load(drawable){ transformations(CircleCropTransformation()) }
+            }
+            itemView.winnerImage.setOnClickListener { cellClickListener.onCellClickListener(winner.first["name"] as String, winner.first["uid"] as String) }
+            if (winner.second["imageUrl"]!=null)
+                itemView.runnerUp1Image.load(winner.second["imageUrl"].toString()) { transformations(CircleCropTransformation()) }
+            else{
+                val firstLetter = winner.second["name"].toString()[0].toString()
+                val secondLetter = winner.second["name"].toString().substring(winner.second["name"].toString().indexOf(' ') + 1, winner.second["name"].toString().indexOf(' ') + 2).trim { it <= ' ' }
+                val drawable = TextDrawable.builder()
+                        .beginConfig()
+                        .width(200)
+                        .height(200)
+                        .endConfig()
+                        .buildRect(firstLetter + secondLetter, Color.DKGRAY)
+                itemView.runnerUp1Image.load(drawable){ transformations(CircleCropTransformation()) }
+            }
+            itemView.runnerUp1Image.setOnClickListener { cellClickListener.onCellClickListener(winner.second["name"] as String, winner.second["uid"] as String) }
+            if (winner.third["imageUrl"]!=null)
+                itemView.runnerUp2Image.load(winner.third["imageUrl"].toString()) { transformations(CircleCropTransformation()) }
+            else{
+                val firstLetter = winner.third["name"].toString()[0].toString()
+                val secondLetter = winner.third["name"].toString().substring(winner.third["name"].toString().indexOf(' ') + 1, winner.third["name"].toString().indexOf(' ') + 2).trim { it <= ' ' }
+                val drawable = TextDrawable.builder()
+                        .beginConfig()
+                        .width(200)
+                        .height(200)
+                        .endConfig()
+                        .buildRect(firstLetter + secondLetter, Color.DKGRAY)
+                itemView.runnerUp2Image.load(drawable){ transformations(CircleCropTransformation()) }
+            }
+            itemView.runnerUp2Image.setOnClickListener { cellClickListener.onCellClickListener(winner.third["name"] as String, winner.third["uid"] as String) }
             itemView.messagesCount.text = winner.messageCount.toString()
             itemView.heartsCount.text = winner.heartsCount.toString()
         }
